05be0bfc8e20e5f69e6b38bdbb05d23a68fa8a00
Tom <littlefirestudiodevelopment@gmail.com>
2017-02-20 19:09:18 +0000

    Broadcast command finalised
    The broadcast command has been finalised however is usable by all user in it's current state, the process of adding admin only to commands is slowly coming along,

--- DiscordBot/DiscordBot/Program.cs
+++ DiscordBot/DiscordBot/Program.cs
@@ -24,7 +24,7 @@ namespace DiscordBot
                 x.AppName = "DiscordBot";
                 x.AppUrl = "";
                 x.LogLevel = LogSeverity.Info;
-                x.LogHandler = Log;
+                x.LogHandler = ClientLog;
             });
 
             Client.UsingCommands(x =>
@@ -47,33 +47,50 @@ namespace DiscordBot
         public void CreateCommands()
         {
             var CService = Client.GetService<CommandService>();
+            // Ping command to test the bot is working
             CService.CreateCommand("ping")
                 .Description("Check the bot is in channel")
                 .Do(async (e) =>
                 {
                     await e.Channel.SendMessage("pong");
+                    Log("[PING]...[PONG]");
                 });
+            // Send a message to the entire server
             CService.CreateCommand("Broadcast")
                 .Alias("bc", "BC", "Announce")
                 .Description("Broadcast/Announce something to all channels")
                 .Parameter("Message", ParameterType.Required)
                 .Do(async (e) =>
                 {
+                    for (int i = 0; i < e.User.Roles.ToArray().Length; i++)
+                    {
+                        var rank = e.User.Roles.ToArray()[i].ToString();
+                        Log($"User rank[{i}]: {rank}");
+                        if (rank != "Admin" && rank != "Owner" && rank != "Co-Owner" && rank != "Developer")
+                        {
+                            await e.Channel.SendMessage($"@{e.User} You do not have access to that command!");
+                        }
+                    }
                     //Console.WriteLine($"Broadcasting: \"{e.Args[0]}\" to channel: \"{e.Channel.Name}\"");
                     //await e.Channel.SendMessage($"[BROADCAST] {e.Args[0]}");
                     for (int i = 0; i < e.Server.ChannelCount; i++)
                     {
                         var ch = e.Server.TextChannels.ToArray()[i];
                         await ch.SendMessage($"[BROADCAST] {e.Args[0]}");
-                        Console.WriteLine($"[BROADCASTING] {e.Args[0]} to channel: {ch.Name}");
+                        Log($"[BROADCASTING] {e.Args[0]} to channel: {ch.Name}");
                     }
                 });
         }
 
         // Write to console the required log info
-        public void Log(object sender, LogMessageEventArgs e)
+        public void ClientLog(object sender, LogMessageEventArgs e)
         {
             Console.WriteLine($"[{e.Severity}] [{e.Source}] {e.Message}");
+        }
+
+        public void Log(string Message)
+        {
+            Console.WriteLine(Message);
         }
     }
 }
