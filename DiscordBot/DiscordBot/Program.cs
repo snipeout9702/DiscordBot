36ea202fd5a2c7b6f22aeeb2fc718634780782b7
Tom <littlefirestudiodevelopment@gmail.com>
2017-02-20 11:08:56 +0000

    Added main bot files
    Added the solution to the repo along with the base code.

---
+++ DiscordBot/DiscordBot/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+
+namespace DiscordBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            new Program().Start();
+        }
+
+        DiscordClient Client;
+
+        public void Start()
+        {
+            Client = new DiscordClient(x =>
+            {
+                x.AppName = "DiscordBot";
+                x.AppUrl = "";
+                x.LogLevel = LogSeverity.Info;
+                x.LogHandler = Log;
+            });
+
+            Client.UsingCommands(x =>
+            {
+                x.PrefixChar = char.Parse("!");
+                x.AllowMentionPrefix = true;
+                x.HelpMode = HelpMode.Public;
+            });
+
+            string Token = "MjgyOTkwMzg4NjkwNTUwNzg1.C4vVxg.5mGFuAKxUkEBknIRfZDBfD4Y_-U";
+
+            CreateCommands();
+
+            Client.ExecuteAndWait(async () =>
+            {
+                await Client.Connect(Token, TokenType.Bot);
+            });
+        }
+
+        public void CreateCommands()
+        {
+            var CService = Client.GetService<CommandService>();
+            CService.CreateCommand("ping")
+                .Description("Check the bot is in channel")
+                .Do(async (e) =>
+                {
+                    await e.Channel.SendMessage("pong");
+                });
+            CService.CreateCommand("Google")
+                .Alias("g", "G")
+                .Description("Do a Google search for something");
+        }
+
+        // Write to console the required log info
+        public void Log(object sender, LogMessageEventArgs e)
+        {
+            Console.WriteLine($"[{e.Severity}] [{e.Source}] {e.Message}");
+        }
+    }
+}
